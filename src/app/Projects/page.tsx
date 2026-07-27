792f0d11fa7aaaa2cded1d70cefdf2e9bc90edd2
PerMariu <104063938+PerMariu@users.noreply.github.com>
2026-07-27 06:07:49 +0200

    Add Nordhordaland Kampsport project image

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -86,7 +86,7 @@ const projects = [
   title: "Nordhordaland Kampsport",
   description:
     "A modern and responsive website for Nordhordaland Kampsport, designed to make it easy for visitors to explore the club, view martial arts activities, find training schedules and access membership information.",
-  image: "/projects/Kampsport/nordhordaland-Kampsport.png",
+  image: "/projects/nordhordaland-Kampsport.png",
   tech: [
     "React",
     "Next.js",
